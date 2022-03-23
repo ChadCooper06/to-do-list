43bca876277f1395c3c662a3bdd3c42d7fb15955
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-23 15:52:46 +0000

    added bottom of page

---
+++ src/bottom.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PageBottom() {
+
+    return(
+        <div className='bottom'>
+            <div className="counter">displays left to do</div>
+            <button className="clearAll">Clear All</button>
+        </div>
+    )
+}
