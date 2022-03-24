c982f6893cdabaa1407023a17bd2989dd41d6ee4
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-24 20:35:20 +0000

    trying to get delete set up

--- src/paper.js
+++ src/paper.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./button";
+import { useState } from "react";
 
 
 export default function Paper({ input, setInput, todos, setTodos }) {
 	
-   
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let newState = todos;
+        let id = Date.now();
+        newState.push({text: input, id: id, status:'active'})
+        setTodos([...newState])
+    }
+    const allTodos = (e) => {
+        setTodos(todos)
+     }
+       
     const onInput = (e) => {
         setInput(e.target.value);
         }
@@ -22,32 +33,54 @@ export default function Paper({ input, setInput, todos, setTodos }) {
         ]);
         setInput('');
     };
+
+    const [input, setInput] = useState()
     
     return (
+        <>
         <div className="paper">
             <h2 className= 'title'>To-Do List</h2>
-            <div className='add-task'>
-            <input
-                type='text'
-                id='userInput'
-                placeholder='Add Task'
-                value={input}
-                name='text'
-                className='input'
-                onChange={onInput}
-            />
+            <div className='add-task' onSubmit={handleSubmit}>
+                <input onChange={e => setInput(e.target.input)} placeholder="Add a task" />
+                <Button text='Add Me!' onClick={onClick} />
+            </div>
             
-            <Button text='Add Me!' onClick={onClick} />
-            
-        </div>
             <div className="selectors">
-                <Button color='green' text='All' onClick={onClick} />
-                <Button color='green' text='Active' onClick={onClick} />
-                <Button color='green' text='Done' onClick={onClick} />
+                <Button color='green' text='All' onClick={allTodos} />
+                <Button color='green' text='active' onClick={activeTodos} />
+                <Button color='green' text='done' onClick={completed} />
             </div>
         </div>
-    );
-};
+        </>
+    )
+}
+   
+    
+    // return (
+    //     <div className="paper">
+    //         <h2 className= 'title'>To-Do List</h2>
+    //         <div className='add-task'>
+    //         <input
+    //             type='text'
+    //             id='userInput'
+    //             placeholder='Add Task'
+    //             value={input}
+    //             name='text'
+    //             className='input'
+    //             onChange={onInput}
+    //         />
+
+            {/* <Button text='Add Me!' onClick={onClick} /> */}
+            
+    //     </div>
+    //         <div className="selectors">
+    //             <Button color='green' text='All' onClick={allTodos} />
+    //             <Button color='green' text='active' onClick={onClick} />
+    //             <Button color='green' text='done' onClick={onClick} />
+    //         </div>
+    //     </div>
+    // );
+//};
 
 
      
