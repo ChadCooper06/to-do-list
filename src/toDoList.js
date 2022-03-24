29460f2f44550cbbbf0e3b4e0027ee418fc5027c
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-24 19:01:47 +0000

    correcting placement of hooks

--- src/toDoList.js
+++ src/toDoList.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css'
 import { Todos } from './todos';
 
-export default function ToDoList({ todos }) {
+export default function ToDoList({ todos, deleteHandler }) {
         //maps over the array of todos(even if empty) and displays them 
     return (
         <div className="todo-container">
             <div className="todo-list">
                {todos.map((todo) => (
-                   <Todos key={todo.id} text={todo.text} />
+                   <Todos key={todo.id} text={todo.text} todos={todos} deleteHandler={deleteHandler}/>
                ))}
             </div>
         </div>
--- src/todos.js
+++ src/todos.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {FaTimes} from 'react-icons/fa';
 
 export const Todos = ({ text }) => {
 
+    const [todos, setTodos] = useState([])
+   
+    const deleteHandler = () => {
+        setTodos(todos.filter(item => item.id !== todos.id))
+     
+     }
+       
+     const checkHandler = () => {
+       todos.completed === false ? (todos.completed = true) :
+       (todos.completed = false)
+     }
 
     return(
         <div className='list-container'>
             <div className='list'>
-                <input type='checkbox'></input>
-                {text}
+                <input type='checkbox' className='check' onClick={checkHandler}></input>
+                {text} 
+                <FaTimes 
+                style={{justifyContent: 'space-between'}} 
+                onClick={() => deleteHandler(todos)} 
+                />
             </div>
         
         </div>
