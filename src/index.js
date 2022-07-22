b46ddfba091a994fc1dc00ec94ce16568d8bd844
ChadCooper06 <chadcooper06@gmail.com>
2022-07-22 15:02:28 +0000

    added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './app';
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />,
+  </Router>,
   rootElement
 );
